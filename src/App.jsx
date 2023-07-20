b2af906aab6ebabf07f2f282d4aab0bc6b7d0f07
Facundo Kameyha <kameyhafacundo@gmail.com>
2023-07-20 17:25:01 -0300

    Agregamos el componente para FormularioColor

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { Container } from "react-bootstrap";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import FormularioColor from "./components/FormularioColor";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Container className="my-5 mainPage">
+        <FormularioColor></FormularioColor>
+      </Container>
+      <footer className="text-center bg-dark text-light py-4">
+        <a href="https://github.com/KameyhaFacundo" className="text-secondary">
+          Facundo Kameyha
+        </a>
+        <p>&copy; Todos los derechos reservados</p>
+      </footer>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/FormularioColor.jsx
@@ -0,0 +1,34 @@
+import { Form, Button, Card } from "react-bootstrap";
+// import ListaColores from "./ListaColores";
+import { useState } from "react";
+
+const FormularioColor = () => {
+  const [color, setColor] = useState("");
+  const [colores, setColores] = useState([]);
+
+  return (
+    <>
+      <Card body className="my-3">
+        <h1 className="text-center text-dark">Administrar Colores</h1>
+        <hr className=" border-1" />
+
+        <Form>
+          <Form.Group className="mb-3 d-flex" controlId="color">
+            <Form.Control
+              type="text"
+              placeholder="Ingrese un color ej: blue"
+              onChange={(e) => setColor(e.target.value)}
+            ></Form.Control>
+            <Button variant="dark mx-2" type="submit">
+              Guardar
+            </Button>
+          </Form.Group>
+        </Form>
+      </Card>
+      <hr className="border-1" />
+      {/* <ListaColores colores={colores} borrarColor={borrarColor}></ListaColores> */}
+    </>
+  );
+};
+
+export default FormularioColor;
